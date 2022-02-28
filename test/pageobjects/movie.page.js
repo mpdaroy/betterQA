28d126dc86be534899efe93115d48bc3f31f86cd
Mark Daroy <mpdaroy@yahoo.com>
2022-02-28 16:16:59 +0800

    BetterQA Demo

---
+++ test/pageobjects/movie.page.js
@@ -0,0 +1,149 @@
+
+import Page from './page';
+
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+
+
+class LoginPage extends Page {
+  
+    /**
+     * a method to verify movie tiles with titles
+     *
+     */
+    async tiles () {  
+        const movieTiles = "//div[2]/h2";  
+        let arrTitles = [];     
+        const arrDiv = await $$(movieTiles);   
+        for (const elem of arrDiv) {
+            // console.log(await elem.getText()); //- for debugging
+            arrTitles.push(await elem.getText())
+        } 
+        //console.log(arrTitles); //- for debugging
+        return(arrTitles) ;
+    }
+
+    /**
+     * a method to verify movie release dates for a specific movie
+     * e.g movieDates(title)
+     * returns Release Date
+     */
+    async movieDates (title) {  
+        const movieDivs = "//div[@class='movies']/div";
+        const releaseDate = "//div[@role='document']//div[2]/div/input";     
+        let relDate = "";  
+
+        const arrDiv = await $$(movieDivs);          
+        for (const elem of arrDiv) {
+            let textTitle = await elem.$$("div")[1].$("h2");            
+
+            let txtTitle = await textTitle.getText();
+            //console.log("Title Here " +  await txtTitle); //- for debugging
+            //console.log("Title Here 2 " +  await title); //- for debugging
+
+            if(txtTitle === title){
+                await elem.$$("div")[2].$("button").click();             
+                await browser.pause(1000);
+                relDate = await $(releaseDate).getAttribute('value');   
+                //console.log("Release Date " +  relDate);    //- for debugging          
+                break;
+            }
+        }
+        $("///div[@role='document']//button[@type='button']").click();
+        return relDate;
+    }
+    /**
+     * a method to verify movie searches and display results correctly
+     *  e.g movieSearch(title)
+     *  returns array of search results of movie titles
+     */
+    async movieSearch (title) {  
+        const inputSearch = "//input[@type='search']"
+        const movieTiles = "//div[2]/h2";  
+        let arrSearchTitles = [];  
+        
+        await $(inputSearch).setValue(title);
+        await browser.keys("Enter")
+        await browser.pause(1000);
+
+        const arrDiv = await $$(movieTiles);   
+        for (const elem of arrDiv) {
+            // console.log(await elem.getText()); //- for debugging
+            arrSearchTitles.push(await elem.getText())
+        } 
+        //console.log(await arrSearchTitles); //- for debugging
+        return(arrSearchTitles) ;
+    }
+
+    /**
+     * a method to verify movie details for a specific movie
+     * e.g movieDetails(title)
+     * returns Movie details for Release Date, Popularity
+    */
+    async movieDetails (title) {  
+        const movieDivs = "//div[@class='movies']/div";
+        let arrDetails = [];    
+        const arrDiv = await $$(movieDivs);          
+
+        for (const elem of arrDiv) {
+            let textTitle = await elem.$$("div")[1].$("h2");
+            let txtTitle = await textTitle.getText();
+            //console.log("Title Here " +  await txtTitle); //- for debugging
+            //console.log("Title Here 2 " +  await title); //- for debugging
+
+            if(txtTitle === title){
+                await elem.$$("div")[2].$("button").click();             
+                await browser.pause(1000);
+
+                for(let i = 2; i <=5; i++){
+                    let releaseDate = "//div[@role='document']//div[" +  i + "]/div/input";  
+                    let strValue = await $(releaseDate).getAttribute('value');
+                    arrDetails.push(await strValue);
+                }
+                break;
+            }
+        }
+        $("///div[@role='document']//button[@type='button']").click();
+        //console.log("Movie Details Here " +  arrDetails); //- for debugging
+        return arrDetails;
+
+    }
+
+    async verify404Images(title) {  
+        await this.movieSearch(title);
+        await browser.pause(1000);
+        const movieDivs = "//div[@class='movies']/div/div[1]"; 
+        let arrImgDetails = []; 
+
+        const arrDiv = await $$(movieDivs);          
+        for (const elem of arrDiv) {       
+
+            let txtTitle = await elem.getAttribute('style');
+            arrImgDetails.push(await txtTitle);
+        }
+        //console.log("Movie Details Here " + arrImgDetails); //- for debugging
+        return arrImgDetails;
+    }
+
+    /**
+     * a method to returns data retrieved after search cancel
+     * e.g verifySearchCancel(title)
+     * returns Movie details for Release Date, Popularity
+    */
+    async verifySearchCancel (title) {  
+        await this.movieSearch(title);
+        await browser.pause(1000);
+        await $("//input[@type='search']").click();
+        //esc key to cancel -->  UI is using webkit which is can't be dealt with XPATH, thus workaround with sendKeys
+        await browser.keys("Escape"); 
+        await browser.pause(2000);
+        await browser.keys("Enter");
+        let arrCancel = await this.tiles(); // getting movie titles after cancelling to be compared to defaults
+        // console.log(arrCancel);  // - for debugging
+        return (arrCancel);
+    }
+}
+
+export default new LoginPage();
---
+++ test/specs/movie.js
@@ -0,0 +1,115 @@
+const expectChai = require('chai').expect;
+import Movie from  '../pageobjects/movie.page';
+import Data from  '../../data/movie.json';
+
+
+
+describe('Movie functional regression tests', () => {
+    it('Should be able to open the application and make sure a list of movie tiles is displayed', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrData = await Movie.tiles();
+    
+        //validate names of the movies and compare to array (expected JSON of movie titles)
+        let x = 0;
+        for (const field of arrData) {                     
+            //console.log(field);  //- for debugging
+            // console.log(Data.movietitle[x])    //- for debugging          
+            expectChai(field).to.equal(Data.movietitle[x]);
+            x = x + 1;              
+        }
+    });
+
+    it('Should show correctly movie release date', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let releaseDate = await Movie.movieDates(Data.movieReleaseDate.title);
+        expectChai(releaseDate).to.equal(Data.movieReleaseDate.date); // validate it correctly set the Release Date
+    }); 
+    
+    it('Should be able to search movie titles correctly', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrResult = await Movie.movieSearch(Data.movieSearch[0]);
+        let isearch = 0;
+        let iNOTsearch = 0;
+        //console.log(arrResult);  //- for debugging  
+        for (const field of arrResult) {                     
+            
+            if(await field === Data.movieSearch[1]){
+                isearch = isearch + 1; 
+            }
+            if(await field === Data.movieSearch[2]){
+                iNOTsearch = iNOTsearch + 1; 
+            }              
+        }
+        expectChai(isearch).to.equal(1);    // Verify for search to be able show expected search
+        expectChai(iNOTsearch).to.equal(0); // Verify for search should NOT be able to show NOT expected search result      
+    }); 
+
+    it('Should show movie details correctly', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrResult = await Movie.movieDetails(Data.movieDetails[0]);
+        
+        //validate details of the movies and compare to array (expected JSON of movie details)
+        let x = 1;
+        for (const field of arrResult) {                     
+            //console.log(field);  //- for debugging
+            //console.log(Data.movieDetails[x])    //- for debugging          
+            expectChai(field).to.equal(Data.movieDetails[x]);
+            x = x + 1;              
+        }
+
+    }); 
+
+    // Did you encounter any bugs in the application? If so add a test that shows that bug
+    // test start here
+    it('should show all search results with valid and not broken images - Expected Fail', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrResult = await Movie.verify404Images(Data.movieSearch[0]);
+
+        let x = 0;
+        for (const field of arrResult) {                     
+            //console.log(field);  //- for debugging       
+            expectChai(field).to.have.string('http://');
+            expectChai(field).to.have.string('.jpg');
+        }
+    });
+
+    it('clicking x(cancel) on search, should get back to default movie lists - Expected Fail', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrDefaultData = await Movie.tiles();  //default array of movie titles
+        let arrSearchCancel = await Movie.verifySearchCancel(Data.movieSearch[0]);
+        let x = 0;
+        for (const field of arrDefaultData) {                     
+            //console.log(field);  //- for debugging
+            //console.log(await arrSearchCancel[x])    //- for debugging          
+            if (await field === await arrSearchCancel[x]){
+                x = x + 1; 
+            }            
+        }
+        expectChai(x).to.be.equal(arrDefaultData.length);
+    });
+
+    it('searching for blank, should get back to default movie lists - Expected Fail', async () => {
+        await Movie.open();
+        await browser.pause(2000);
+        let arrDefaultData = await Movie.tiles();  //default array of movie titles
+        let arrSearchBlank = await Movie.movieSearch();
+        let x = 0;
+        for (const field of arrDefaultData) {                     
+            //console.log(field);  //- for debugging
+            //console.log(await arrSearchBlank[x])    //- for debugging          
+            if (await field === await arrSearchBlank[x]){
+                x = x + 1; 
+            }            
+        }
+        expectChai(x).to.be.equal(arrDefaultData.length);
+    }); 
+
+});
+
+
